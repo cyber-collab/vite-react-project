ce843f591e9911729c723f07c79fd14f3974645e
Den <denyszkramar@gmail.com>
2024-05-20 15:57:40 +0300

    feat: add bad counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import './App.css'
-import {Header} from "./component/Header.tsx";
-import {Button} from "./component/Button.tsx";
-import {useState} from "react";
+import {Header} from "./components/Header.tsx";
+import {Button} from "./components/Button.tsx";
+import {useEffect, useState} from "react";
 
 function App() {
-const [counter, setCounter] = useState(0);
+// const [counter, setCounter] = useState(0);
+
+useEffect(()=> {
+    // console.log(counter)
+})
+
+const useCounter = () => {
+        const [value, setState] = useState(0);
+        const increment = () => setState(value + 1)
+        return {value, increment}
+    }
+
+const [state, setState] = useState({isAdmin: false, userName: 'Bla bla'});
+const {isAdmin} = state;
+const assignAdmin = () => {
+    setState({...state, isAdmin: true});
+};
 
   return (
     <>
-      <Header title={'Counter: ' + counter} description={''} />
-        <Button onClick={() => setCounter(counter + 1)} text={'Press to button'}/>
+      <Header title={'Counter: '} description={''} />
+        <Button onClick={() => useCounter.increment} text={'Press to button'}/>
+        <h1>Hello, user is {isAdmin ? "super user" : 'simple user'}</h1>
+        <button onClick={assignAdmin}></button>
     </>
   )
 }
